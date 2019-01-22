c991f92f5f27f7ddf4c954f39258c91e18edf0d4
Shehryar Kamran <kool_dude1996@live.com>
2019-01-22 18:01:39 +0500

    Initial commit

---
+++ Client_GUI/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Win_frm_Tcp_client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(17, 248);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(218, 247);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(95, 23);
+            this.Send.TabIndex = 8;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(20, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 148);
+            this.listBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(221, 37);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(95, 23);
+            this.Connect.TabIndex = 5;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            // 
+            // client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 306);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Connect);
+            this.Name = "client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Connect;
+    }
+}
+
---
+++ server_GUI/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace win_frm_tcp_server
+{
+    partial class Tcp_seerver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Send = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Listen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(191, 251);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(89, 22);
+            this.Send.TabIndex = 7;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(49, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 164);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Listen
+            // 
+            this.Listen.Location = new System.Drawing.Point(49, 7);
+            this.Listen.Name = "Listen";
+            this.Listen.Size = new System.Drawing.Size(231, 36);
+            this.Listen.TabIndex = 4;
+            this.Listen.Text = "Listen";
+            this.Listen.UseVisualStyleBackColor = true;
+            this.Listen.Click += new System.EventHandler(this.Listen_Click);
+            // 
+            // Tcp_seerver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 291);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Listen);
+            this.Name = "Tcp_seerver";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Listen;
+    }
+}
+
